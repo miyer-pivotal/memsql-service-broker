ef12482fa021ba9c666dad8bea48823e2f7849e9
Mallika Iyer <miyer@pivotal.io>
2016-04-26 15:27:31 -0400

    added globlal vars:

--- src/main/java/org/cf/cloud/servicebroker/memsql/config/MemSQLConfig.java
+++ src/main/java/org/cf/cloud/servicebroker/memsql/config/MemSQLConfig.java
@@ -11,24 +11,24 @@ import java.sql.Connection;
 @Configuration
 public class MemSQLConfig {
 
-	//@Value("${memsql.user}")
-    @Value("${MEMSQL_USERNAME}")
+	@Value("${memsql.user}")
+    //@Value("${MEMSQL_USERNAME}")
 	private String username;
 	
-	//@Value("${memsql.password}")
-    @Value("${MEMSQL_PASSWORD}")
+	@Value("${memsql.password}")
+    //@Value("${MEMSQL_PASSWORD}")
     private String password;
     
-    //@Value("${memsql.host}")
-    @Value("${MEMSQL_HOST}")
+    @Value("${memsql.host}")
+    //@Value("${MEMSQL_HOST}")
     private String host;
     
-    //@Value("${memsql.port}")
-    @Value("${MEMSQL_PORT}")
+    @Value("${memsql.port}")
+    //@Value("${MEMSQL_PORT}")
     private int port = 3306;
     
-    //@Value("${memsql.database}")
-    @Value("${MEMSQL_DATABASE}")
+    @Value("${memsql.database}")
+    //@Value("${MEMSQL_DATABASE}")
     private String database;
     
 	@Bean
